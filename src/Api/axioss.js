e9cd1fc8b226efd77cfadf424af0bc53a59da072
ermiyasayeledeveloper <tiobura772@gmail.com>
2025-09-19 23:46:58 -0700

    axios modified

--- src/Api/axioss.js
+++ src/Api/axioss.js
@@ -5,6 +5,8 @@ const axiosInstance = axios.create({
    // baseURL: "http://127.0.0.1:5001/alone-fa57b/us-central1/api"
    //deployed
    baseURL: "https://amazon-api-deploy-tjf3.onrender.com"
+   //no function
+   // baseURL: "http://localhost:5000"
 
 })
 
